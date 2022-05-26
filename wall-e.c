e36aabe6ac4b355828f7ae1c15013fa14742bdc0
Renzo Salcedo <salcedo_salcedo.renzo@courrier.uqam.ca>
2022-05-25 23:56:12 -0400

    add du .doc et refactoring

--- wall-e.c
+++ wall-e.c
@@ -1,38 +1,45 @@
 /*  INF3173 - TP1
  *  Session : été 2022
- *  
+ *
  *  IDENTIFICATION.
  *
- *      Nom : Salcedo 	
+ *      Nom : Salcedo
  *      Prénom : Renzo A.
  *      Code permanent : SALR02089408
  *      Groupe : 40
  */
- 
+
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <ctype.h>
 
-const int DIR_MODE1 = 0700;
-const int DIR_MODE2 = 0755;
 
-void valider_dir(char *path,const int dir_mode) {
+const int DIR_MODE1 = 0700;  //Mode des droits complets pour l'usager seulement.
+const int DIR_MODE2 = 0755;  //Mode demandé dans le TP.
+const int ARG_1 = 1;  //1 argument passé au programme
+const int ARG_2 = 2;  //2 argument passé au programme
+
+/**
+ * validation des repertoires, arrete le programme lorsque validation correspondante precise.
+ *
+ * @param chemin pointeur vers le chemin a ecrire
+ * @param argv pointeur vers les arguments.
+ */
+void valider_dir(char *chemin, const int dir_mode) {
     struct stat st = {0};
-    if (stat(path, &st) == -1) {
-        if (mkdir(path, dir_mode) != 0)
+    if (stat(chemin, &st) == -1) {
+        if (mkdir(chemin, dir_mode) != 0)
             exit(1);
-    } else if (dir_mode == DIR_MODE2 && stat(path, &st) == 0)
+    } else if (dir_mode == DIR_MODE2 && stat(chemin, &st) == 0)
         exit(1);
 }
 
-/*
-* Valide le nb arguments recus par le programme. Affiche l'erreur correspndate et arrête le programme s'il y a plus
-* qu'un argument valide
+/**
+* Valide le nb arguments recus par le programme. Arrête le programme s'il y a plus que 2 args.
 *
 * @param argc nb d'arguments.
 * */
@@ -41,70 +48,122 @@ void valider_nb_args(int argc) {
         exit(1);
 }
 
+/**
+ * Ouvre le fichier passer en argument pour lecture.
+ *
+ * @param argv pointeur vers les arguments.
+ * */
 FILE *ouvrir_fichier(char *argv[]) {
-    FILE *file = fopen(argv[1], "r");
+    FILE *file = fopen(argv[ARG_1], "r");
     if (file == NULL)
         exit(1);
     return file;
 }
 
-void ecrire_fichier(char *path, int taille) {
-    int fd = open(path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    write(fd, path, (size_t) taille);
-}
-
-char *creer_path(char *ligne, char *argv[]) {
-    char *word = strtok(ligne, " \n\0");
-    for (int i = 0; i < 2; ++i)
-        word = strtok(NULL, " \n\0");
-    char path[strlen(argv[2]) + strlen(word) + 2];
-    snprintf(path, sizeof(path), "%s%s%s", argv[2], "/", word);
-    return strdup(path);
-}
-
+/**
+ * Obtient la taille dun fichier. Regulier, repertoire ou symbolique
+ *
+ * @param ligne pointeur vers la ligne lu a manipuler.
+ */
 int obtenir_taille(const char *ligne) {
-    char *taille = strstr(ligne, " ") + 1;
+    char *taille = strstr(ligne, " ") + 1; //Deplacer le pointeur de +1 charactere après l'espace.
     int i;
     for (i = 0; isdigit(taille[i]); i++);
-    char subbuff[i];
-    memcpy(subbuff, taille, i);
-    return atoi(subbuff);
+    char sub_tampon[i];
+    memcpy(sub_tampon, taille, i);
+    return atoi(sub_tampon);
 }
 
-void creer_repertoire(char *ligne, char *argv[]) {
-    int taille = (int) obtenir_taille(ligne);
-    char *path = creer_path(ligne, argv);
-    valider_dir(path, DIR_MODE2);
-    ecrire_fichier(path, taille);
-    free(path);
+/**
+ * Cree un string dun chemin desiré
+ *
+ * @param ligne pointeur vers la ligne lu a manipuler.
+ * @param argv pointeur vers les arguments.
+ */
+char *creer_path(char *ligne, char *argv[]) {
+    char *mot = strtok(ligne, " \n\0");
+    for (int i = 0; i < 2; ++i)
+        mot = strtok(NULL, " \n\0");
+    char chemin[strlen(argv[ARG_2]) + strlen(mot) + 2]; //+2 pour les characteres "/" et \0 (fin de ligne)
+    snprintf(chemin, sizeof(chemin), "%s%s%s", argv[2], "/", mot);
+    return strdup(chemin);
 }
 
-void ecrire_fichier_symbol(char *path, char *ligne) {
+/**
+ * Ecrit un lien symbolique
+ *
+ * @param chemin pointeur vers le chemin a ecrire
+ * @param ligne pointeur vers la ligne lu a manipuler.
+ */
+void ecrire_fichier_symbol(char *chemin, char *ligne) {
     ligne[strlen(ligne) - 1] = '\0';
-    char word [strlen(strstr(ligne, ">") + 2)];
-    memcpy(word, strstr(ligne, ">") + 2, strlen(strstr(ligne, ">") + 2));
-    if (symlink(word, path))
+    char mot [strlen(strstr(ligne, ">") + 2)]; //Deplacer le pointeur de +2 characteres après le charctere > vers le 1er charactere du mot suivant.
+    memcpy(mot, strstr(ligne, ">") + 2, strlen(strstr(ligne, ">") + 2));
+    if (symlink(mot, chemin))
         exit(1);
 }
 
-void creer_lsymbolique(char *ligne, char *argv[]) {
-    char teste [strlen(ligne)];
-    strcpy(teste, ligne);
-    char *path = creer_path(teste, argv);
-    struct stat buf;
-    if (lstat(path, &buf) == 0)
-        unlink(path);
-    ecrire_fichier_symbol(path, ligne);
-    free(path);
+/**
+ * Ecrit un fichier.
+ *
+ * @param chemin pointeur vers le chemin a ecrire
+ * @param taille du fichier a ecrire.
+ */
+void ecrire_fichier(char *chemin, int taille) {
+    int fd = open(chemin, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    write(fd, chemin, (size_t) taille);
 }
 
+/**
+ * Cree un repertoire
+ *
+ * @param ligne pointeur vers la ligne lu a manipuler.
+ * @param argv pointeur contenant les arguments.
+ */
+void creer_repertoire(char *ligne, char *argv[]) {
+    int taille = (int) obtenir_taille(ligne);
+    char *chemin = creer_path(ligne, argv);
+    valider_dir(chemin, DIR_MODE2);
+    ecrire_fichier(chemin, taille);
+    free(chemin);
+}
+
+/**
+ * Cree un lien symbolique
+ *
+ * @param ligne pointeur vers la ligne lu a manipuler.
+ * @param argv pointeur contenant les arguments.
+ */
+void creer_lsymbolique(char *ligne, char *argv[]) {
+    char ligne_copie [strlen(ligne)];
+    strcpy(ligne_copie, ligne);
+    char *chemin = creer_path(ligne_copie, argv);
+    struct stat tampon;
+    if (lstat(chemin, &tampon) == 0)
+        unlink(chemin);
+    ecrire_fichier_symbol(chemin, ligne);
+    free(chemin);
+}
+
+/**
+ * Cree un fichier regulier.
+ *
+ * @param ligne pointeur vers la ligne lu a manipuler.
+ * @param argv pointeur contenant les arguments.
+ */
 void creer_fregulier(char *ligne, char *argv[]) {
     int taille = (int) obtenir_taille(ligne);
-    char *path = creer_path(ligne, argv);
-    ecrire_fichier(path, taille);
-    free(path);
+    char *chemin = creer_path(ligne, argv);
+    ecrire_fichier(chemin, taille);
+    free(chemin);
 }
 
+/**
+ * Lecture du fichier.
+ *
+ * @param file pointeur vers le fichier a lire
+ * @param argv pointeur contenant les arguments.
+ */
 void lire_file(FILE *file, char *argv[]) {
     size_t len = 0;
     char *ligne;
@@ -118,9 +177,15 @@ void lire_file(FILE *file, char *argv[]) {
     }
 }
 
-int main(int argc, char ** argv) {
+/**
+ * Méthode main, lance tous les sous-methodes correspondate.
+ *
+ *@param argc nb d'arguments.
+ *@param argv pointeur contenant les arguments.
+*/
+int main(int argc, char *argv[]) {
     valider_nb_args(argc);
-    valider_dir(argv[2], DIR_MODE1);
+    valider_dir(argv[ARG_2], DIR_MODE1);
     FILE *file = ouvrir_fichier(argv);
     lire_file(file, argv);
     return 0;
